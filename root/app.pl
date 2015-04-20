eb7aa4a6321ce63acc6b4784703381119bf35ec3
Daniel <daniel@telebid-pro.com>
2015-04-20 18:51:34 +0300

    encoding fix + apache conf

--- root/app.pl
+++ root/app.pl
@@ -3,9 +3,9 @@ use lib qw(/home/daniel/test_system_project/test_me-server/lib/perl);
 
 use TestMe::WebHandle::WebHandle;
 
-binmode (STDIN, ":utf8");
-binmode (STDOUT, ":utf8");
-binmode (STDERR, ":utf8");
+#binmode (STDIN, ":utf8");
+#binmode (STDOUT, ":utf8");
+#binmode (STDERR, ":utf8");
 
 if(shift)
 {
